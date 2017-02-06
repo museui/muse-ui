e0f8e13a6c4b50983ad0edc58899b51de8b5559c
Kiyan Gauss <fourcels@gmail.com>
2017-02-06 14:16:03 +0800

    fix vue-i18n issue

--- src-docs/views/components/icon/api.js
+++ src-docs/views/components/icon/api.js
@@ -3,16 +3,16 @@ export default {
     name: 'value',
     type: 'String',
     default: '',
-    desc: 'icon的名称，不同的value 对应不同的icon， 可以去图表集中查找'
+    desc: 'props.value'
   }, {
     name: 'size',
     type: 'Number',
     default: '24',
-    desc: '图标的大小'
+    desc: 'props.size'
   }, {
     name: 'color',
     type: 'String',
     default: '',
-    desc: '图表的颜色'
+    desc: 'props.color'
   }]
 }
