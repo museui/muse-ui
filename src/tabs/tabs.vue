6a9745bcf7256bece89debaadf10747661d371ee
guzhentao <438766114@qq.com>
2018-01-22 15:52:07 +0800

    remve handleTabClick useless  method in Tabs component

--- src/tabs/tabs.vue
+++ src/tabs/tabs.vue
@@ -7,77 +7,71 @@
 
 <script>
 export default {
-  name: 'mu-tabs',
+  name: "mu-tabs",
   props: {
     lineClass: {
       type: [String, Object, Array]
     },
     value: {}
   },
-  data () {
+  data() {
     return {
       tabLightStyle: {
-        width: '100%',
-        transform: 'translate3d(0, 0, 0)'
+        width: "100%",
+        transform: "translate3d(0, 0, 0)"
       }
-    }
+    };
   },
-  updated () {
-    this.setTabLightStyle()
+  updated() {
+    this.setTabLightStyle();
   },
   methods: {
-    handleTabClick (value, tab) {
-      if (this.value !== value) {
-        this.$emit('change', value)
-      }
-    },
-    getActiveIndex () {
-      if (!this.$children || this.$children.length === 0) return -1
-      let activeIndex = -1
+    getActiveIndex() {
+      if (!this.$children || this.$children.length === 0) return -1;
+      let activeIndex = -1;
       this.$children.forEach((tab, i) => {
         if (tab.value === this.value) {
-          activeIndex = i
-          return false
+          activeIndex = i;
+          return false;
         }
-      })
-      return activeIndex
+      });
+      return activeIndex;
     },
-    setTabLightStyle () {
-      const x = this.getActiveIndex() * 100
-      const len = this.$children.length
-      const el = this.$refs.highlight
-      el.style.width = len > 0 ? (100 / len).toFixed(4) + '%' : '100%'
-      el.style.transform = 'translate3d(' + x + '%, 0, 0)'
+    setTabLightStyle() {
+      const x = this.getActiveIndex() * 100;
+      const len = this.$children.length;
+      const el = this.$refs.highlight;
+      el.style.width = len > 0 ? (100 / len).toFixed(4) + "%" : "100%";
+      el.style.transform = "translate3d(" + x + "%, 0, 0)";
     }
   },
-  mounted () {
-    this.setTabLightStyle()
+  mounted() {
+    this.setTabLightStyle();
   }
-}
+};
 </script>
 
 <style lang="less">
 @import "../styles/import.less";
-.mu-tabs{
+.mu-tabs {
   display: flex;
   justify-content: space-between;
   align-items: center;
   background-color: @primaryColor;
-  color: #FFF;
+  color: #fff;
   text-align: center;
   position: relative;
   z-index: 100;
   width: 100%;
 }
 
-.mu-tab-link-highlight{
+.mu-tab-link-highlight {
   position: absolute;
   left: 0;
   bottom: 0;
   height: 2px;
   background-color: @accentColor;
-  transition: transform .3s;
+  transition: transform 0.3s;
   backface-visibility: hidden;
 }
-
 </style>
