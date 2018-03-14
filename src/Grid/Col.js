bcef521e39733e18cfcc3e5907d3ae120b199468
myronliu347 <myronliu347@gmail.com>
2018-03-14 14:01:11 +0800

    add bootstrap4 grid layout

--- src/Grid/Col.js
+++ src/Grid/Col.js
@@ -1,19 +1,36 @@
+import { props, generatePropsClass } from './utils';
+
+function createColClass (props) {
+  const classNames = [];
+  if (props.span) classNames.push('col-' + props.span);
+  if (props.sm) classNames.push('col-sm-' + props.sm);
+  if (props.md) classNames.push('col-md-' + props.md);
+  if (props.lg) classNames.push('col-lg-' + props.lg);
+  if (props.xl) classNames.push('col-xl-' + props.xl);
+  if (props.order) classNames.push('order-' + props.order);
+  if (props.offset) classNames.push('offset-' + props.offset);
+  return classNames.join(' ');
+}
+
 export default {
   name: 'mu-col',
   functional: true,
   props: {
-    width: {
-      type: String,
-      default: '100'
-    },
-    tablet: String,
-    desktop: String
+    tag: String,
+    alignSelf: props.alignSelf,
+    fill: Boolean,
+    span: [String, Number], // auto 1-12
+    sm: [String, Number], // auto 1-12
+    md: [String, Number], // auto 1-12
+    lg: [String, Number], // auto 1-12
+    xl: [String, Number], // auto 1-12
+    order: [String, Number], // first last 0-12
+    offset: [String, Number] // 1-11
   },
   render (h, { data, props, children }) {
-    const classNames = [`col-${props.width}`];
-    if (props.tablet) classNames.push(`tablet-${props.tablet}`);
-    if (props.desktop) classNames.push(`desktop-${props.desktop}`);
-    data.staticClass = `col ${data.staticClass || ''} ${classNames.join('')}`;
-    return h('div', data, children);
+    const flex = generatePropsClass(props);
+    const col = createColClass(props);
+    data.staticClass = ['col', col, flex, data.staticClass || ''].join(' ');
+    return h(props.tag || 'div', data, children);
   }
 };
