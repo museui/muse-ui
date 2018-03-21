d030ecd59af5809a5a2d841206dda1d65564d8a6
myronliu347 <myronliu347@gmail.com>
2018-03-21 09:42:57 +0800

    eslint

--- src/DataTable/mixins/colgroup.js
+++ src/DataTable/mixins/colgroup.js
@@ -15,23 +15,23 @@ export default {
       const checkboxColWidth = 75;
       let tableElWidth = this.$el.offsetWidth;
       const widthArr = this.columns.filter((column) => column.width).map((column) => Number(column.width));
-      widthArr.forEach((width) => tableElWidth -= width);
+      widthArr.forEach((width) => (tableElWidth -= width));
       let otherWidth = Math.floor(tableElWidth / (this.columns.length - widthArr.length));
       if (otherWidth < minWidth) otherWidth = minWidth;
       this.cols = this.columns.map((column) => {
         return column.width ? column.width : this.fit ? otherWidth : minWidth;
-      })
+      });
       if (this.checkbox) this.cols.unshift(checkboxColWidth);
 
       if (!this.fit) {
         let tableWidth = 0;
-        this.cols.forEach((width) => tableWidth += Number(width));
+        this.cols.forEach((width) => (tableWidth += Number(width)));
         this.tableWidth = tableWidth + 'px';
       } else {
         this.tableWidth = '';
       }
     },
-    createColGroup(h) {
+    createColGroup (h) {
       return h('colgroup', {
         directives: [{
           name: 'resize',
@@ -43,7 +43,7 @@ export default {
             width
           }
         });
-      }))
+      }));
     }
   },
   watch: {
