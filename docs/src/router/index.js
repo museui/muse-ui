14728c0525d6d4a4f9a75b0ae4e19906b0a45957
myronliu347 <myronliu347@gmail.com>
2018-04-25 17:57:28 +0800

    eslint

--- docs/src/router/index.js
+++ docs/src/router/index.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from 'vue';
+import Router from 'vue-router';
+import HelloWorld from '@/components/HelloWorld';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
@@ -12,4 +12,4 @@ export default new Router({
       component: HelloWorld
     }
   ]
-})
+});
