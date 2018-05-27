1ce09619889dac9a77a108954d65c24ec5078165
myronliu347 <myronliu347@gmail.com>
2018-05-27 16:39:41 +0800

    config

--- docs/config/index.js
+++ docs/config/index.js
@@ -38,7 +38,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'https://www.muse-ui.org/',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
