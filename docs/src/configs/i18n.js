be27df4ce5745cea0741eb0bc4cd89c12b539ba7
Jerry Zhang <endday@users.noreply.github.com>
2018-05-28 10:52:59 +0800

    docs: 修复了首页的文案错误

--- docs/src/configs/i18n.js
+++ docs/src/configs/i18n.js
@@ -13,7 +13,7 @@ const config = {
     featureTitle1: '组件丰富',
     featureContent1: 'Muse UI 拥有40多个UI 组件，用于适应不同业务环境。',
     featureTitle2: '可定制',
-    featureContent2: 'Muse UI 自定义主题方式及为优雅，仅需少量代码即可完成主题样式替换。',
+    featureContent2: 'Muse UI 自定义主题方式极为优雅，仅需少量代码即可完成主题样式替换。',
     featureTitle3: '基于 Vue 2.0',
     featureContent3: 'Muse UI 基于 Vue2.0 开发，Vue2.0是当下最快的前端框架之一，小巧，api友好，可用于开发的复杂单页应用',
     install: '安装',
