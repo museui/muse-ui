3f0b13ad9fe883f8baaac0754013509cf5d33275
myronliu347 <myronliu347@gmail.com>
2018-06-12 22:25:22 +0800

    fixed https://github.com/museui/muse-ui/issues/962

--- ui/Helpers/index.js
+++ ui/Helpers/index.js
@@ -4,6 +4,8 @@ import {
   FadeTransition,
   SlideTopTransition,
   SlideBottomTransition,
+  SlideLeftTransition,
+  SlideRightTransition,
   ScaleTransition
 } from '../internal/transitions';
 import clickOutside from '../internal/directives/click-outside';
@@ -18,6 +20,8 @@ export default {
       FadeTransition,
       SlideTopTransition,
       SlideBottomTransition,
+      SlideLeftTransition,
+      SlideRightTransition,
       ScaleTransition
     ].forEach(transition => Vue.component(transition.name, transition));
     Vue.directive(clickOutside.name, clickOutside);
