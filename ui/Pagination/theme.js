938abbba6318c9f7c98e562f87a5615f855dabbc
myronliu347 <myronliu347@gmail.com>
2018-06-12 11:40:48 +0800

    feat: pagination add cirle raised

--- ui/Pagination/theme.js
+++ ui/Pagination/theme.js
@@ -7,5 +7,9 @@ export default (theme) => {
   .mu-pagination-item.mu-button.is-current {
     background-color: ${theme.primary};
   }
+  .mu-pagination__raised .mu-pagination-item.mu-button,
+  .mu-pagination__raised .mu-pagination-btn.mu-button{
+    background-color: ${theme.text.alternate};
+  }
   `;
 };
