f1fd265b0ae4b8a171f7f2d3b332dd38e3497186
myronliu347 <myronliu347@gmail.com>
2018-06-16 00:37:21 +0800

    feat: carbon theme

--- docs/src/theme.js
+++ docs/src/theme.js
@@ -170,8 +170,45 @@ MuseUI.theme.add('carbon', {
     disabled: 'rgba(0, 0, 0, 0.38)',
     hint: 'rgba(0, 0, 0, 0.38)'
   },
-  divider: '#edeff2'
+  divider: '#d3d6db'
 });
 MuseUI.theme.addCreateTheme(markdownTheme);
 MuseUI.theme.addCreateTheme(highlightTheme);
 MuseUI.theme.addCreateTheme(appTheme);
+MuseUI.theme.addCreateTheme((theme, type, name) => {
+  return name === 'carbon' ? `
+  .mu-item.is-selected,
+  .mu-slider,
+  .mu-breadcrumbs-item,
+  .mu-radio-checked,
+  .mu-form-item__focus,
+  .mu-checkbox-checked,
+  .mu-switch-checked,
+  .mu-picker,
+  .mu-input__focus,
+  .mu-refresh-control,
+  .mu-app-drawer-header .mu-appbar-title-text:hover,
+  .mu-app-drawer-header .mu-app-version:hover,
+  .mu-bottom-item-active .mu-bottom-item-icon,
+  .mu-bottom-item-active .mu-bottom-item-text {
+    color: ${theme.secondary};
+  }
+  .mu-picker-display,
+  .mu-datetime-picker .mu-tabs {
+    background-color: ${theme.primary};
+  }
+  .mu-timepicker-number__selected,
+  .mu-linear-progress-background,
+  .mu-linear-progress-indeterminate,
+  .mu-linear-progress-determinate,
+  .mu-pagination-item.mu-button.is-current {
+    background-color: ${theme.secondary};
+  }
+  .mu-circle-spinner {
+    border-color: ${theme.secondary};
+  }
+  .mu-circular-progress-determinate-path{
+    stroke: ${theme.secondary};
+  }
+  ` : '';
+});
