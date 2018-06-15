076c267846ada85d2aa76cd610d2b850c1765f21
myronliu347 <myronliu347@gmail.com>
2018-06-15 18:43:05 +0800

    button ui fix

--- ui/Button/theme.js
+++ ui/Button/theme.js
@@ -6,12 +6,6 @@ export default (theme) => {
     background-color: ${theme.background.paper};
     color: ${theme.text.primary};
   }
-  .mu-raised-button.hover .mu-button-wrapper {
-    background-color: ${fade(theme.text.primary, 0.1)};
-  }
-  .mu-raised-button.mu-inverse.hover  .mu-button-wrapper {
-    background-color: ${fade(theme.text.alternate, 0.3)};
-  }
   .mu-raised-button.disabled{
     color: ${fade(theme.text.primary, 0.3)};
     background-color: ${darken(theme.text.alternate, 0.1)};
@@ -19,20 +13,10 @@ export default (theme) => {
   .mu-flat-button {
     color: ${theme.text.primary};
   }
-  .mu-flat-button.hover,
-  .mu-icon-button.hover {
-    background-color: ${fade(theme.text.primary, 0.1)};
-  }
   .mu-flat-button.disabled {
     color: ${theme.text.disabled};
   }
-  .mu-flat-button .mu-circle-ripple {
-    color: ${theme.text.primary};
-  }
   .mu-icon-button {
-    color: ${theme.text.primary};
-  }
-  .mu-icon-button .mu-circle-ripple{
     color: ${theme.text.primary};
   }
   .mu-icon-button.disabled {
@@ -41,10 +25,6 @@ export default (theme) => {
   .mu-fab-button {
     background-color: ${theme.primary};
     color: ${theme.text.alternate};
-  }
-  .mu-fab-button.hover .mu-button-wrapper,
-  .mu-fab-button:active .mu-button-wrapper {
-    background-color: ${fade(theme.text.alternate, 0.3)};
   }
   .mu-fab-button.disabled {
     color: ${fade(theme.text.primary, 0.3)};
