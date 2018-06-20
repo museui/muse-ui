2f3f38169768d874a1b12b0eaa8a5b59a92c448e
myronliu347 <myronliu347@gmail.com>
2018-06-20 10:49:48 +0800

    search

--- docs/src/components/search.js
+++ docs/src/components/search.js
@@ -1,15 +1,48 @@
 import algoliasearch from 'algoliasearch';
 export default {
+  props: {
+    data: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data () {
+    return {
+      searchText: ''
+    };
+  },
   created () {
     const client = algoliasearch('RGHCT36SHC', 'f38ef54caaee2e01cbd18a077325dee1');
-    this.index = client.initIndex('your_index_name');
+    this.index = client.initIndex('getstarted_actors');
+  },
+  methods: {
+    search (text) {
+      if (!text) return;
+      this.index.search({ query: text, hitsPerPage: 6 }, (err, res) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log(res);
+      });
+    }
   },
   render (h) {
     return h('mu-auto-complete', {
       staticClass: 'mu-docs-search',
       props: {
         solo: true,
-        icon: 'search'
+        icon: 'search',
+        value: this.searchText
+      },
+      on: {
+        input: (val) => {
+          this.searchText = val;
+          if (this.timer) clearTimeout(this.timer);
+          this.timer = setTimeout(() => this.search(this.searchText), 500);
+        },
+        select: (value, item) => {
+        }
       }
     });
   }
