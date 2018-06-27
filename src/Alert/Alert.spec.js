2efcd7f81a88c29bc5ad3333b95f2528ecfcd212
myronliu347 <myronliu347@gmail.com>
2018-06-27 13:53:17 +0800

    feat: alert test

--- src/Alert/Alert.spec.js
+++ src/Alert/Alert.spec.js
@@ -1,9 +1,27 @@
 import test from '../utils/testing';
 import Alert from './Alert';
 
-test('Alert.vue', ({ mount }) => {
-  it('show be show by default', async () => {
+test('Alert', ({ mount }) => {
+  it('should be show by default', async () => {
     const wrapper = mount(Alert, {});
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+  it('should be delete', async () => {
+    const wrapper = mount(Alert, { propsData: { delete: true }});
+    const deleteBtn = wrapper.find('.mu-alert-delete-btn')[0];
+    const handleDelete = jest.fn();
+    wrapper.vm.$on('delete', handleDelete);
+    deleteBtn.trigger('click');
+    expect(handleDelete).toBeCalledWith();
+  });
+  it('should be set color', async () => {
+    const wrapper = mount(Alert, {
+      propsData: {
+        color: 'primary'
+      }
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+    wrapper.setProps({ color: 'error' });
     expect(wrapper.html()).toMatchSnapshot();
   });
 });
