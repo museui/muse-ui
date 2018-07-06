a046f7f359b33e37fea0451e578c398c492b1ba8
myronliu347 <myronliu347@gmail.com>
2018-07-06 23:30:39 +0800

    fix:  colgroup width fixed https://github.com/museui/muse-ui/issues/1041

--- src/DataTable/mixins/colgroup.js
+++ src/DataTable/mixins/colgroup.js
@@ -1,5 +1,15 @@
 import resize from '../../internal/directives/resize';
 export default {
+  props: {
+    minColWidth: {
+      type: Number,
+      default: 128
+    },
+    checkboxColWidth: {
+      type: Number,
+      default: 75
+    }
+  },
   data () {
     return {
       cols: [],
@@ -11,27 +21,24 @@ export default {
   },
   methods: {
     setCols () {
-      const minWidth = 128;
-      const checkboxColWidth = 75;
       let tableElWidth = this.$el.offsetWidth;
       const widthArr = this.columns.filter((column) => column.width).map((column) => Number(column.width));
       widthArr.forEach((width) => (tableElWidth -= width));
+      if (this.checkbox) tableElWidth -= this.checkboxColWidth;
+
       let otherWidth = Math.floor(tableElWidth / (this.columns.length - widthArr.length));
-      if (otherWidth < minWidth) otherWidth = minWidth;
+
+      if (otherWidth < this.minColWidth) otherWidth = this.minColWidth;
       this.cols = this.columns.map((column) => {
-        return column.width ? column.width : this.fit ? otherWidth : minWidth;
+        return column.width ? column.width : this.fit ? otherWidth : this.minColWidth;
       });
-      if (this.checkbox) this.cols.unshift(checkboxColWidth);
+      if (this.checkbox) this.cols.unshift(this.checkboxColWidth);
       let tableWidth = 0;
       this.cols.forEach((width) => (tableWidth += Number(width)));
       this.tableWidth = tableWidth + 'px';
     },
     createColGroup (h) {
       return h('colgroup', {
-        // directives: [{
-        //   name: 'resize',
-        //   value: this.setCols
-        // }]
       }, this.cols.map((width) => {
         return h('col', {
           attrs: {
