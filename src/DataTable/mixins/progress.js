8fc8f419b5c75c1ddf6eefa1b21ef1ccc76bc215
myronliu347 <myronliu347@gmail.com>
2018-07-14 14:29:52 +0800

    fix: error null

--- src/DataTable/mixins/progress.js
+++ src/DataTable/mixins/progress.js
@@ -2,21 +2,16 @@ import { LinearProgress } from '../../Progress';
 import { FadeTransition } from '../../internal/transitions';
 
 export default {
-  data () {
-    return {
-      headerHeight: ''
-    };
-  },
   mounted () {
-    this.headerHeight = this.$refs.header.offsetHeight + 'px';
   },
   methods: {
     createProgress (h) {
+      const headerHeight = this.$refs.header ? this.$refs.header.offsetHeight + 'px' : '';
       return h(FadeTransition, {}, [
         this.loading ? h(LinearProgress, {
           staticClass: 'mu-table-progress',
           style: {
-            top: this.headerHeight
+            top: headerHeight
           }
         }) : undefined
       ]);
