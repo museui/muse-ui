428d411190bbce50d7af8d17a2a3870da203302a
myronliu347 <myronliu347@gmail.com>
2018-07-14 14:29:43 +0800

    build: move docs code to muse-docs

--- demo/main.js
+++ demo/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import MuseUI from '../ui';
+import MuseUI from '../src';
 import App from './App';
 Vue.use(MuseUI);
 
--- docs/src/configs/lang.js
+++
@@ -1,9 +0,0 @@
-export default [{
-  lang: 'zh-CN',
-  desc: '简体中文',
-  img: (size) => `https://countryflags.io/cn/flat/${size}.png`
-}, {
-  lang: 'en-US',
-  desc: 'English',
-  img: (size) => `http://www.countryflags.io/US/flat/${size}.png`
-}];
