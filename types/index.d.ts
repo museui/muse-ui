68577f4cd09afd80ae4794f59e66c8da5e197ef5
myronliu347 <myronliu347@gmail.com>
2018-07-29 15:41:16 +0800

    fix: 1099

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,9 +4,11 @@ import * as MuseUI from './muse-ui';
 export default MuseUI;
 
 declare module 'muse-ui/lib/theme' {
+  import * as MuseUI from './muse-ui';
   export default MuseUI.theme;
 }
 
 declare module 'muse-ui/lib/theme/colors' {
+  import * as MuseUI from './muse-ui';
   export default MuseUI.Colors;
 }
