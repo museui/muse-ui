c590a2115f22389c485916aec090d53313ab2187
myronliu347 <myronliu347@gmail.com>
2018-08-31 17:25:16 +0800

    fix: ts bugs fixed https://github.com/museui/muse-ui/issues/1163

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,12 +3,12 @@ export * from './muse-ui';
 import * as MuseUI from './muse-ui';
 export default MuseUI;
 
-declare module 'muse-ui/lib/theme' {
+declare module './muse-ui/lib/theme' {
   import * as MuseUI from './muse-ui';
   export default MuseUI.theme;
 }
 
-declare module 'muse-ui/lib/theme/colors' {
+declare module './muse-ui/lib/theme/colors' {
   import * as MuseUI from './muse-ui';
   export default MuseUI.Colors;
 }
